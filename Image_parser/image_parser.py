6d3b4581c2f204a8e231ec205a9e99bf5f86752e
linkcla <linkcla10@gmail.com>
2023-11-09 11:18:24 +0100

    [NEW] Python program that parse an image to X68 sintax

---
+++ Image_parser/image_parser.py
@@ -0,0 +1,21 @@
+from skimage.io import imread
+
+theImage = imread('./Image_parser/PACMAN.png')
+print(theImage.shape)
+cont = 0
+for row in range(theImage.shape[0]):
+    for column in range(theImage.shape[1]):
+
+        red = theImage[row, column, 0]
+        green = theImage[row, column, 1]
+        blue = theImage[row, column, 2]
+
+        if cont % 6 == 0:
+            print('$00%.2X%.2X%.2X'%(blue, green, red), end="")
+        else:
+            print('$00%.2X%.2X%.2X,'%(blue, green, red), end="")
+        if cont % 6 == 0:
+            print('\n DC.L      ', end="")
+
+        cont += 1
+        
